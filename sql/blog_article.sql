2b4a408a8eddc8d609441c862c264b84eb78cc48
蔡江 <caijiang@kuaifawu.com>
2018-12-28 10:47:07 +0800

    push table

---
+++ sql/blog_article.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `blog_article` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(60) NOT NULL DEFAULT '' COMMENT '标题',
+  `category_id` int(11) NOT NULL DEFAULT '0' COMMENT '类别id群',
+  `author_id` int(10) unsigned NOT NULL DEFAULT '1' COMMENT '作者ID',
+  `remove` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除 0/否1/是2/待发布',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
