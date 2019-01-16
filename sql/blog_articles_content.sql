b88c48c8f09ca609af5b3b8d6ee5cba82e4b2afa
蔡江 <caijiang@kuaifawu.com>
2019-01-16 10:37:18 +0800

    sql文件

--- sql/blog_articles_content.sql
+++ sql/blog_articles_content.sql
@@ -2,6 +2,7 @@ CREATE TABLE `blog_articles_content` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `article_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '文章ID',
   `content` text COMMENT '文章内容',
+  `html_content` text,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
